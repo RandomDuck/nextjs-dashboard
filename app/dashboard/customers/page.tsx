238a70e412e78f605729b0107042f774d89d0d8b
Johannes Ström <johannes.strom@svenskaspel.se>
2024-02-18 08:00:33 +0100

    Extra credit: Fixed customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,12 +1,27 @@
 import { fetchFilteredCustomers } from '@/app/lib/data';
 import CustomersTable from '@/app/ui/customers/table';
+import Pagination from '@/app/ui/invoices/pagination';
 import { Metadata } from 'next';
  
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default async function page() {
-  const customers = await fetchFilteredCustomers('');
-  return <CustomersTable customers={customers}/>
+export default async function page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+  return <div>
+      <CustomersTable customers={customers}/>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={1} />
+      </div>
+    </div>
 }
